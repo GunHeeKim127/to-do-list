a4a45cd2eed90a70edc314c6dd992b6f51d77c49
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2026-09-21 14:09:22 +0900

    2026.09.21. route 에러 수정

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,1 +1,8 @@
-    const groqModelName = process.env.GROQ_MODEL_NAME || "openai/gpt-oss-120b";
+const groqModelName = process.env.GROQ_MODEL_NAME || "openai/gpt-oss-120b";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        message: "API 정상 작동",
+    });
+}
